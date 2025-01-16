fa545128b90818896f64b5f8b4678a4dbf954c3d
gaelrf <gaelrey99@gmail.com>
2025-01-16 16:50:21 +0100

    registro

---
+++ registro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="usuario">Usuario</label>
+        <input type="text" name="usuario" id="usuario" placeholder="Usuario" required>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" placeholder="Correo" required>
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" id="password" placeholder="Contraseña" required>
+        <input type="submit" value="Registrarse">
+    </form>
+    
+</body>
+</html>
